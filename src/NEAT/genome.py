b82b6ed35dba306b25021b88e467763865eaca32
Rafferty Chen <raffertychen@gmail.com>
2020-08-26 17:55:14 -0700

    Add docstrings to genome

--- src/NEAT/genome.py
+++ src/NEAT/genome.py
@@ -5,6 +5,10 @@ from typing import List, Sequence
 from connection_gene import ConnectionGene
 
 
+INPUTS = 240 * 256
+OUTPUTS = 12
+
+
 class Genome:
     def __init__(self):
         self.genes: List[ConnectionGene] = []
@@ -12,16 +16,29 @@ class Genome:
 
     @classmethod
     def crossover(cls, parent1: Genome, parent2: Genome) -> Genome:
+        """
+        Creates a new genome from two parent genomes. parent1 should be the fitter parent.
+        Genes that are shared by both parents are randomly selected to go into the new child.
+        Disjoint genes are taken from parent1 (the fitter parent).
+        """
         pass
 
     def is_same_species(self, other: Genome) -> bool:
         pass
 
     def mutate(self) -> None:
+        """
+        Randomly mutates the genome in a number of different ways:
+        - For each connection, perturb each weight
+        - Add a connection
+        - Add a node
+        """
         pass
 
     def generate_network(self) -> None:
+        """Generate the neural network based on the connection genes"""
         pass
 
     def evaluate_input(self, input: Sequence[float]) -> List[float]:
+        """Runs the input through the generated neural network. generate_network must be run first."""
         pass
