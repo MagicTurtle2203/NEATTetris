eb9cab34d3117523ea079c5a11dfeaa7d04fa34f
Rafferty Chen <raffertychen@gmail.com>
2020-08-30 19:20:18 -0700

    Remove bias from ConnectionGene

--- src/NEAT/connection_gene.py
+++ src/NEAT/connection_gene.py
@@ -1,8 +1,7 @@
 class ConnectionGene:
-    def __init__(self, in_node: int, out_node: int, weight: float, bias: float, enabled: bool, innovation_number: int):
+    def __init__(self, in_node: int, out_node: int, weight: float, enabled: bool, innovation_number: int) -> None:
         self.in_node = in_node
         self.out_node = out_node
         self.weight = weight
-        self.bias = bias
         self.enabled = enabled
         self.innovation_number = innovation_number
