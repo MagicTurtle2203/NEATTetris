b3164e39dfe8b5e1384cbb5768585b9cdeeb37fc
Rafferty Chen <raffertychen@gmail.com>
2020-08-30 21:11:43 -0700

    Add equality check for ConnectionGene

--- src/NEAT/connection_gene.py
+++ src/NEAT/connection_gene.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class ConnectionGene:
     def __init__(self, in_node: int, out_node: int, weight: float, enabled: bool, innovation_number: int) -> None:
         self.in_node = in_node
@@ -5,3 +8,6 @@ class ConnectionGene:
         self.weight = weight
         self.enabled = enabled
         self.innovation_number = innovation_number
+
+    def __eq__(self, other: ConnectionGene) -> bool:
+        return self.in_node == other.in_node and self.out_node == other.out_node
