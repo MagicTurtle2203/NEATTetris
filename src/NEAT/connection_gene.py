249990ae247b6eedee2f1d8830fbbc600d2321c3
Rafferty Chen <raffertychen@gmail.com>
2020-08-31 00:32:44 -0700

    Add default arguments to ConnectionGene

--- src/NEAT/connection_gene.py
+++ src/NEAT/connection_gene.py
@@ -2,12 +2,11 @@ from __future__ import annotations
 
 
 class ConnectionGene:
-    def __init__(self, in_node: int, out_node: int, weight: float, enabled: bool, innovation_number: int) -> None:
+    def __init__(self, in_node: int, out_node: int, weight: float = 0.0, enabled: bool = True) -> None:
         self.in_node = in_node
         self.out_node = out_node
         self.weight = weight
         self.enabled = enabled
-        self.innovation_number = innovation_number
 
     def __eq__(self, other: ConnectionGene) -> bool:
         return self.in_node == other.in_node and self.out_node == other.out_node
