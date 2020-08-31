8b3e6bed6ec8f64eb8919fa33eac64ae5a68357c
Rafferty Chen <raffertychen@gmail.com>
2020-08-30 20:44:43 -0700

    Combine node classes into one

--- src/NEAT/node.py
+++ src/NEAT/node.py
@@ -1,39 +1,21 @@
 from __future__ import annotations
 
-from typing import Union
+from typing import List
 
 
 class Node:
     node_id = 0
-
-    def __init__(self):
-        self.id = Node.node_id
-        Node.node_id += 1
-
-
-class InputNode(Node):
-    def __init__(self):
-        super().__init__()
-        self.value = 0
-
-    def __repr__(self) -> str:
-        return f"InputNode(value={self.value})"
-
-    def calculate_value(self) -> float:
-        return self.value
-
-
-class ComputeNode(Node):
     cache = {}
 
     def __init__(self):
-        super().__init__()
-        self.inputs = []
-        self.weights = []
+        self.inputs: List[Node] = []
+        self.weights: List[float] = []
         self.bias = 0
 
-    def __repr__(self) -> str:
-        return f"ComputeNode(inputs={self.inputs}, weights={self.weights}, bias={self.bias})"
+        self.value = 0
+
+        self.id = Node.node_id
+        Node.node_id += 1
 
     @classmethod
     def reset_cache(cls) -> None:
@@ -47,25 +29,24 @@ class ComputeNode(Node):
     def _relu(x: float) -> float:
         return max(0, x)
 
-    def add_input(self, in_node: Union[ComputeNode, InputNode], weight: float) -> None:
+    def add_input(self, in_node: Node, weight: float) -> None:
         self.inputs.append(in_node)
         self.weights.append(weight)
 
     def calculate_value(self) -> float:
+        if len(self.inputs) == 0:
+            return self.value
+
         assert self.weights is not None and len(self.weights) == len(
             self.inputs
         ), "The number of weights must match the number of inputs"
 
         output = sum(
-            (
-                input_node.calculate_value()
-                if input_node.id not in ComputeNode.cache
-                else ComputeNode.cache[input_node.id]
-            )
+            (input_node.calculate_value() if input_node.id not in Node.cache else Node.cache[input_node.id])
             * self.weights[idx]
             for idx, input_node in enumerate(self.inputs)
         )
 
-        ComputeNode.cache[self.id] = self._relu(output + self.bias)
+        Node.cache[self.id] = self._relu(output + self.bias)
 
-        return ComputeNode.cache[self.id]
+        return Node.cache[self.id]
