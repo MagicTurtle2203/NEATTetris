7260056ea06a1aa6c56fc9a6e279ebca492d8caa
Rafferty Chen <raffertychen@gmail.com>
2020-09-01 00:26:53 -0700

    Add copy method to ConnectionGene

--- src/NEAT/connection_gene.py
+++ src/NEAT/connection_gene.py
@@ -1,18 +1,28 @@
 from __future__ import annotations
 
-from typing import Dict, Tuple
+from typing import Dict, Optional, Tuple
 
 
 class ConnectionGene:
     innovation_cache: Dict[Tuple[int, int], int] = {}
     innovation_number = 0
 
-    def __init__(self, in_node: int, out_node: int, weight: float = 0.0, enabled: bool = True) -> None:
+    def __init__(
+        self,
+        in_node: int,
+        out_node: int,
+        weight: float = 0.0,
+        enabled: bool = True,
+        innovation_number: Optional[int] = None,
+    ) -> None:
         self.in_node = in_node
         self.out_node = out_node
         self.weight = weight
         self.enabled = enabled
-        self.innovation_number = self._get_innovation_number()
+        self.innovation_number = self._get_innovation_number() if innovation_number is None else innovation_number
+
+    def __copy__(self) -> ConnectionGene:
+        return type(self)(self.in_node, self.out_node, self.weight, self.enabled, self.innovation_number)
 
     def __eq__(self, other: ConnectionGene) -> bool:
         return self.in_node == other.in_node and self.out_node == other.out_node
